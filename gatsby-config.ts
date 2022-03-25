63d126df8baa6f9e75cc0164d4cebb873df24f39
hijack2ee <hi.jack2ee@gmail.com>
2022-03-26 02:13:59 +0900

    feat(config): resolve source filesystem, page creator for mdx

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,7 +1,14 @@
-import type { GatsbyConfig } from "gatsby"
+import type {GatsbyConfig} from 'gatsby';
+
+const cwd = process.cwd();
 
 const config: GatsbyConfig = {
-  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-mdx'],
-}
+  plugins: [
+    'gatsby-plugin-emotion',
+    {resolve: 'gatsby-source-filesystem', options: {name: 'contents', path: `${cwd}/src/contents`}},
+    {resolve: 'gatsby-plugin-page-creator', options: {path: `${cwd}/src/contents`}},
+    'gatsby-plugin-mdx',
+  ],
+};
 
-export default config
+export default config;

26a519cd0955d706b0e39ad703de162f95b8b734
hijack2ee <hi.jack2ee@gmail.com>
2023-08-26 00:06:10 +0900

    fix: set direct assetPrefix

--- next.config.js
+++ next.config.js
@@ -2,5 +2,5 @@ const isProd = process.env.NODE_ENV === "production";
 
 /** @type {import('next').NextConfig} */
 module.exports = {
-  basePath: isProd ? "https://hijack2ee.github.io/blog" : undefined,
+  assetPrefix: isProd ? "https://hijack2ee.github.io/blog" : undefined,
 };

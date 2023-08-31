0e5c61ad45710b62c81f97c0376fa41ef762e97d
hijack2ee <hi.jack2ee@gmail.com>
2023-08-31 18:39:00 +0900

    feat: implement common image component

---
+++ components/common/Img.tsx
@@ -0,0 +1,15 @@
+import { DetailedHTMLProps, ImgHTMLAttributes } from "react";
+
+import { concat } from "../../lib/utils/url";
+
+const Img = ({
+  src,
+  ...props
+}: DetailedHTMLProps<
+  ImgHTMLAttributes<HTMLImageElement>,
+  HTMLImageElement
+>) => (
+  <img src={`//${concat(process.env.STATIC_ASSET_CDN_HOST, src)}`} {...props} />
+);
+
+export default Img;
